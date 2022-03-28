5441df5c4257b46b948f5c3a2c3114008c35a1ee
Maxim Kolesov <pro100maks222@gmail.com>
2022-03-28 14:03:30 +0300

    Add stage 1 tests

--- Core/TText.cpp
+++ Core/TText.cpp
@@ -1,1 +1,35 @@
 #include "TText.h"
+
+TText::TText(PTTtextLink pl) {}
+
+void TText::Read(char* pFileName) {
+	throw std::runtime_error("No implementation");
+}
+
+void TText::Write(char* pFileName) {
+	throw std::runtime_error("No implementation");
+}
+
+void TText::Print(std::ostream& stream) {
+	throw std::runtime_error("No implementation");
+}
+
+int TText::GoFirstLink(void) {
+	return DataErr;
+}
+
+int TText::GoNextLink(void) {
+	return DataErr;
+}
+
+int TText::GoPrevLink(void) {
+	return DataErr;
+}
+
+std::string TText::GetLine(void) {
+	throw std::runtime_error("No implementation");
+}
+
+void TText::SetLine(std::string s) {
+	throw std::runtime_error("No implementation");
+}
